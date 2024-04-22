4240efc8897c75a80da0724494d205d0c0c7eac5
jtr <jtr-code>
2024-04-23 00:16:53 +0530

    better comment

--- useEffect/src/components/Example.jsx
+++ useEffect/src/components/Example.jsx
@@ -1,18 +1,25 @@
 import { useEffect, useState } from "react";
 
 const Example = () => {
+  // State variables for user data, loading state, error state, and ID
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
   const [id, setId] = useState(1);
 
-  console.log("user: ", user);
+  /*
+    The AbortController is used to cancel the fetch request if the component unmounts,
+    preventing unnecessary network requests and potential memory leaks.
+    This ensures that only the latest fetch request is processed and older requests are aborted.
+  */
 
+  // Fetch user data when ID changes
   useEffect(() => {
     const BASE_URL = `https://dummyjson.com/users/${id}`;
 
     const controller = new AbortController();
 
+    // Fetch user data from the API
     fetch(BASE_URL, {
       signal: controller.signal,
     })
@@ -24,23 +31,26 @@ const Example = () => {
         }
       })
       .then((data) => {
-        // setTimeout(() => {
+        // Set user data and update loading state
         setUser(data);
         setLoading(false);
-        // }, 1000);
       })
       .catch((err) => {
         throw new Error(err.message);
       });
 
+    // Cleanup function to abort fetch request if component unmounts
     return () => {
       controller.abort();
     };
   }, [id]);
 
+  // Function to generate a random ID when button is clicked
   function handleClick() {
     setId(Math.floor(Math.random() * 100));
   }
+
+  // Render the component with loading, error, and user data handling
   return (
     <div>
       <button onClick={handleClick}>Add some values</button>
